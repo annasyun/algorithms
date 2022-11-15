f9a342bba26297a93464a45bac0d53f315fabe8e
yunjeong.seo <100075245+annasyun@users.noreply.github.com>
2022-11-16 01:30:28 +0900

    [level 0] Title: 중앙값 구하기, Time: 2.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,7 +1,17 @@
+// function solution(array) {
+//     let answer = 0;
+//     array.sort((a,b) => a-b);
+//     let index = array[((array.length)/2).toFixed()-1];
+//     console.log(index);
+//     answer = index;
+//     return answer;
+// }
+
 function solution(array) {
     let answer = 0;
     array.sort((a,b) => a-b);
-    let index = array[((array.length)/2).toFixed()-1];
+    let index = array[Math.floor((array.length)/2)];
+    console.log(index);
     answer = index;
     return answer;
 }
