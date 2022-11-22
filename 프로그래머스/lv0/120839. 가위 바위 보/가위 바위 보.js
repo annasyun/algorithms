83195b12af343a970d679fdaab29857f24ed8522
yunjeong.seo <100075245+annasyun@users.noreply.github.com>
2022-11-22 23:23:10 +0900

    [level 0] Title: 가위 바위 보, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,21 +1,29 @@
+// function solution(rsp) {
+//  const rspArr = rsp.split('');
+
+//     return (rspArr.map((i)=>{
+//     if(i==2){
+//         i=0;
+//     }else if(i==0){
+//         i=5;
+//     }else{
+//         i=2;
+//     }
+// })
+//                ).join('');
+// }
+
 function solution(rsp) {
-    let rspArr = rsp.split("");
-    console.log(rspArr);
-    let newArr = [];
-    for(let i=0;i<rspArr.length;i++){
-        switch (parseInt(rspArr[i])) {
-            case 2:
-                rspArr[i] = 0
-                break;
-            case 0:
-                rspArr[i] = 5
-                break;
-            case 5:
-                rspArr[i] = 2
-                break;
-        }
-        newArr.push(rspArr[i]);
+ const rspArr = rsp.split('');
+let answer = rspArr.map((i)=>{
+    if(i==2){
+        return 0;
+    }else if(i==0){
+        return 5;
+    }else{
+        return 2;
     }
-    console.log(newArr);
-    return newArr.join("");
+});
+    return answer.join('');
 }
+
