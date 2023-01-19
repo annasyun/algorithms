34281eff5f9e7f852265bdbbb8836fe3a7675eac
yunjeong.seo <100075245+annasyun@users.noreply.github.com>
2023-01-19 14:13:21 +0900

    Update 외계행성의 나이.js

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,3 +1,14 @@
 function solution(age) {
   return [...('' + age)].map(num => String.fromCharCode('a'.charCodeAt(0) + +num)).join('');
 }
+
+function solution(age) {
+    const ageArr = String(age).split('');
+    
+    let answer = '';
+    
+    for(let i=0; i< ageArr.length; i++){
+        answer += String.fromCharCode(Number(ageArr[i])+97);
+    }
+    return answer;
+}
