f1f25984f3ef96b4570635979e642d1e689a1339
yunjeong.seo <100075245+annasyun@users.noreply.github.com>
2023-05-17 15:12:20 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,13 +1,8 @@
-// function solution(num) {
-//     let answer = "Even";
-//     if(Math.abs(num%2)===1){
-//         answer = "Odd";
-//     }
-//     return answer;
-// }
-
-
 function solution(num) {
-
-    return num % 2 ? "Odd" : "Even"
+    let answer = "Even";
+    if(Math.abs(num%2)===1){
+        answer = "Odd";
+    }
+    return answer;
 }
+
